3b313df96b6b9e1ef90ad456e98e534684ed1d26
gikuuuajah <gikuwelso@gmail.com>
2025-10-18 22:05:57 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  owner: "6285774306979@s.whatsapp.net", // NOMOR ADMIN UTAMA
+  groupNotif: "kosongg@g.us", // ID GRUP UNTUK NOTIFIKASI
+  pakasirApiKey: "nPsXDdINO6D3bogHmRTX58LQXnI5XLjs",
+  voucherPrice: 2500,
+  checkInterval: 5000, // 30 detik cek status
+  orderTimeout: 30 * 60 * 1000 // 30 menit auto cancel
+};
